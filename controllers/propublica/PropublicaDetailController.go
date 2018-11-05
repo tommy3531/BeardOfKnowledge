23fea2a86ddf87c230539e83092c5e5aa64d1866
tommy3531 <thomas.marler@hotmail.com>
2018-11-04 22:02:26 -0600

    Get leg id from URL, store it in variable and so it to retrieve information about a specific legislator

--- controllers/propublica/PropublicaDetailController.go
+++ controllers/propublica/PropublicaDetailController.go
@@ -1,18 +1,46 @@
 package propublica
 
 import (
-	"fmt"
 	"net/http"
+	"net/url"
 	"html/template"
+	// "encoding/json"
+	// "log"
+	// "html/template"
 	"github.com/gorilla/mux"
 )
 
 func GetSenatorDetails(w http.ResponseWriter, r *http.Request) {
+
 	params := mux.Vars(r)
 	id := params["id"]
-	fmt.Println(id)
+
+	client := &http.Client{}
+	url := url.URL{
+		Scheme: "https",
+		Host:   "api.propublica.org",
+		Path:   "/congress/v1/members/" + id + ".json",
+	}
+	println(url.String())
+	req, _ := http.NewRequest("GET", url.String(), nil)
+
+	req.Header.Add("X-API-KEY", "SpzjlPZlkMlPKKGCLQS1OqZtCN96lPl7sszOTKra")
+	resp, _ := client.Do(req)
+	defer resp.Body.Close()
+
+	// if resp.StatusCode == http.StatusOK {
+	// 	bodyBytes, _ := ioutil.ReadAll(resp.Body)
+	// 	bodyString := string(bodyBytes)
+	// 	fmt.Println(bodyString)
+	// }
+
+	// var responseObject propublicaModel.Politican
+	// if err := json.NewDecoder(resp.Body).Decode(&responseObject); err != nil {
+	// 	log.Println(err)
+	// }
+
 	t, _ := template.ParseFiles("template/showSenatorDetails.html")
-	t.Execute(w, id)
+	t.Execute(w, "")
 }
 
 // func GetHouseDetails() {
